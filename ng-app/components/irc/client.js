60cdff2a3d2cc24d4875756434caa498a5148431
jaywunder <jaywunder@gmail.com>
2015-06-15 10:26:01 -0400

    removed double connection bug

--- ng-app/components/irc/client.js
+++ ng-app/components/irc/client.js
@@ -7,9 +7,6 @@ module.exports = function (app, mainWindow, clientData) {
     port: 6667
   });
 
-  mainWindow.client.connect();
-  //TODO: try commenting this line to see if the dounble connection issue goes away
-
   mainWindow.client.addListener('registered', function () {
     mainWindow.send('connect-ready');
   });
