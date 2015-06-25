fd5ae8eaa41ba936ac0d60c2fc6063edfe056b2f
jaywunder <jaywunder@gmail.com>
2015-06-25 15:25:51 -0400

    fixed applying bug and who knows what else?

--- app/render/js/chat.js
+++ app/render/js/chat.js
@@ -38,20 +38,51 @@ chat.controller('ChatController', function ($scope) {
     $scope.active = name; // point $scope.active to the active channel
   };
 
-  // the order isn't preserved yet, they'll be in alphabetical order
+  // the order of channels isn't preserved yet, they'll be in alphabetical order
   // $scope.joinChannel('#botwar2');
   $scope.joinChannel('#botwar');
   // $scope.joinChannel('#botwar3');
-  // $scope.joinChannel('#botwar4');
-})
 
-.directive('chatTab', function () { //not in use currently
-  return {
-    template:
-    '<button ng-repeat="channel in chat.channels" class="tab tab-active-{{channel.active}}">\n'+
-      '<div class="tab-title" data-name="{{channel.name}}"></div>\n'+
-      '<div class="tab-close-icon"></div>\n'+
-    '</button>'
+  /*
+   * For right now I'm going to store all the messagin methods in this scope.  I don't
+   * like it this way, but the first version is a "good 'nuf" version. So this'll have to do.
+   * Maybe it'll get fixed? 😅
+   */
+
+  $scope.submitMessage = function () {
+    if ($scope.currentMessage !== '') {// prevent sending empty strings
+      client.say('' + $scope.active, '' + $scope.currentMessage); // send message to server
+      $scope.currentMessage = ''; // clear the current message
+    }
+  };
+
+  $scope.message = function (name, nick, text) {
+    var typeNick = nick === client.nick ? 'self' : 'other';
+    // push a message to the active channel's messages array
+    $scope.channels[name].messages.push({
+      typeNick: typeNick, // the css class the nick will be given: either 'self' or 'other'
+      typeMessage: 'text', // the css class the message will be given
+      nick: nick, // include the nickname
+      text: text // include the text
+    });
+    if (typeNick !== 'self') $scope.$apply();
+  };
+
+  $scope.announce = function (name, text) {
+    // push a message to the active channel's messages array
+    $scope.channels[name].messages.push({
+      typeNick: 'none', // have no nickname
+      typeMessage: 'annoucment', // the class the message text will be given
+      nick: '', // annoncments aren't sent by anyone
+      text: text // include the text of the message
+    });
+    $scope.$apply();
+  };
+
+  $scope.testMessage = function () {
+    // test the messages
+    $scope.message($scope.active, 'chester', 'ayy lmao');
+    $scope.announce($scope.active, '$scope is something important');
   };
 })
 
@@ -59,61 +90,20 @@ chat.controller('ChatController', function ($scope) {
   'use strict';
   var $ = require('../lib/jquery-1.11.3.min.js');
   var ipc = require('ipc');
-  var channel = $scope;
   var remote = require('remote');
   var mainWindow = remote.getCurrentWindow();
   var client = mainWindow.client;
 
-  $scope.name = '';
   $scope.currentMessage = '';
 
-  $scope.submitMessage = function () {
-    console.log('trying to submit a message now');
-    if ($scope.currentMessage !== '') {// prevent sending empty strings
-      client.say('' + $scope.name, '' + $scope.currentMessage); // send message to server
-      $scope.currentMessage = ''; // clear the current message
-    }
-  };
-
-  $scope.message = function (nick, text) {
-    console.log('Messaging');
-    var typeNick = nick === client.nick ? 'self' : 'other';
-    // push a message to the active channel's messages array
-    $scope.channels[$scope.active].messages.push({
-      typeNick: typeNick, // the css class the nick will be given: either 'self' or 'other'
-      typeMessage: 'text', // the css class the message will be given
-      nick: nick, // include the nickname
-      text: text // include the text
-    });
-  };
-
-  $scope.announce = function (text) {
-    // push a message to the active channel's messages array
-    $scope.channels[$scope.active].messages.push({
-      typeNick: 'none', // have no nickname
-      typeMessage: 'annoucment', // the class the message text will be given
-      nick: '', // annoncments aren't sent by anyone
-      text: text // include the text of the message
-    });
-  };
-
-  $scope.testMessage = function () {
-    // test the messages
-    var numTests = 3;
-    for (var i = 0; i <= numTests; i++) {
-      $scope.message('chester', 'ayy lmao');
-      $scope.announce('$scope is something important');
-    }
-  };
-
-  $scope.testMessage();
+  // $scope.testMessage();
 
   /////////////////////////
   //BIND TO CLIENT EVENTS//
   /////////////////////////
 
   ipc.on('client-error', function (error) {
-    $scope.message('error: ', error);
+    $scope.message($scope.active, 'error: ', error);
     console.log(error);
   });
 
@@ -122,11 +112,11 @@ chat.controller('ChatController', function ($scope) {
   });
 
   ipc.on('client-selfMessage', function (to, text) {
-    $scope.message(client.nick, text);
+    $scope.message(to, client.nick, text);
   });
 
   ipc.on('client-message', function (nick, to, text, message) {
-    $scope.message(nick, text);
+    $scope.message(to, nick, text);
   });
 
   ipc.on('client-names', function (names) {
@@ -134,7 +124,7 @@ chat.controller('ChatController', function ($scope) {
   });
 
   ipc.on('client-kill', function (nick, reason, channels, message) {
-    $scope.message();
+    // $scope.announce();
   });
 
   ipc.on('client-pm', function (nick, text, message) {
@@ -142,12 +132,18 @@ chat.controller('ChatController', function ($scope) {
   });
 
   ipc.on('client-kick', function () {
-    $scope.announce('You were kicked.. :(');
+    // $scope.announce('You were kicked.. :(');
   });
 
   ipc.on('', function () {
 
   });
+})
+
+.directive('nope', function () {
+  return {
+    template: ''
+  };
 })
 
 .directive('chatPane', function () {
@@ -168,5 +164,15 @@ chat.controller('ChatController', function ($scope) {
         '</form>\n'+
       '</div>\n'+
     '</div>'
+  };
+})
+
+.directive('chatTab', function () { //not in use currently
+  return {
+    template:
+    '<button ng-repeat="channel in chat.channels" class="tab tab-active-{{channel.active}}">\n'+
+      '<div class="tab-title" data-name="{{channel.name}}"></div>\n'+
+      '<div class="tab-close-icon"></div>\n'+
+    '</button>'
   };
 });
---
+++ names.md
@@ -0,0 +1,26 @@
+Prattlebox <-- decided
+==========
+
+
+poundlebox
+
+*-box?*
+
+Imagi
+
+Valinto
+
+wundle
+
+raine
+
+Knocle
+
+Anythier
+
+tombo
+
+amplet
+
+jobberbox // too much like that other protocol  
+jabberbox
