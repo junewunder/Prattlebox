2a9fb8a5abf2f65073d23d97607aa689b7c7fea4
jaywunder <jaywunder@gmail.com>
2015-08-11 15:46:59 -0400

    things probably look better

--- app/render/chat/js/controllers/controller.js
+++ app/render/chat/js/controllers/controller.js
@@ -121,8 +121,12 @@ chat.controller('ChatController', function ($scope) {
 
   $scope.testMessage = function () {
     // test the messages
-    $scope.message($scope.active.name, '😈', 'ayy lmao');
+    $scope.message($scope.channels['#jaywunder'], '😈', 'ayy lmao');
   };
+
+  // for (var i = 0; i < 100; i++) {
+  //   $scope.testMessage();
+  // }
 
   $scope.popUp = function () {
     // will open /render/popup/{filename}/index.html
